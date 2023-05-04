cc5718346f41a99ccb594a2f0951aace19bfa8b0
SpaceManV3 <vmarcinkowskirvms@gmail.com>
2023-05-03 23:39:22 -0700

    try 2

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -11,7 +11,7 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    for (int i = 0; i < n; i++){
       double sum = 0.0;
       for (int j = 0; j < n; j++){
-         sum += A[i * n + j] * x[j];
+         sum += A[i,j] * x[j];
       }
       y[i] = sum;
    }
